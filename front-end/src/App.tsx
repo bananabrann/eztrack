ba5c4be7d29817c694e6926aef20fc9e60cbbcd1
bananabrann <plbrannan95@gmail.com>
2026-02-08 10:32:40 -0600

    Purposely break app

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
 	return (
 		<div className="flex justify-center items-center h-screen">
-			<img src="/eztrack-logo.png" alt="eztrack logo" className="max-w-md" />
-		</div>
+			<img src="/eztrack-logo.png" alt="eztrack logo" className="max-w-md" /
+
+
+	</div>
 	);
 }
 
