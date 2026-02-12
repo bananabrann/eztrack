59208289997a7603f2c87ec9f0ccba6ee9184015
Josh <joshcanterbury.dev@gmail.com>
2026-02-12 00:04:57 -0600

    Added delete project route and functionality.

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 
+import projectRouter from "./routes/projects";
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -8,6 +10,8 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
 	res.json({ message: "Hello World from Backend!" });
 });
+
+app.use("/api/projects", projectRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
---
+++ back-end/src/routes/projects.ts
@@ -0,0 +1,13 @@
+import { Router } from "express"
+
+import { projects } from "../fake/projects";
+
+const router = Router();
+
+// DELETE project endpoint
+
+router.delete("/:id", (req, res) => {
+    return res.status(200).json({message: "Project was deleted successfully."});
+});
+
+export default router;
