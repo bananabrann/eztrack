3d861d44615db677b858c437ce9585a1f480eec1
FernandoDLT <ferchodlt1971@gmail.com>
2026-02-12 20:59:46 -0600

    Run formatter

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -1,51 +1,57 @@
 export default function Header() {
 	return (
 		<header className="w-full border-b">
-			<nav className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4"
-				aria-label="Main Nav"			
+			<nav
+				className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4"
+				aria-label="Main Nav"
 			>
 				{/*Logo*/}
 				<div>
-					<img className="h-8 w-auto"
+					<img
+						className="h-8 w-auto"
 						src="/eztrack-logo.png"
-						alt="EZTrack Company Logo"						
+						alt="EZTrack Company Logo"
 					/>
 				</div>
 
 				{/*Nav Links*/}
 				<ul className="flex space-x-8">
 					<li>
-						<a className="hover:text-[#EA5C1F] focus:outline-none focus:ring-2 focus:ring-[#EA5C1F] rounded"
+						<a
+							className="hover:text-[#EA5C1F] focus:outline-none focus:ring-2 focus:ring-[#EA5C1F] rounded"
 							href="/"
-                     aria-label="Go to homepage"
-                  >
+							aria-label="Go to homepage"
+						>
 							Home
 						</a>
 					</li>
 
 					<li>
-						<a className="hover:text-[#EA5C1F] focus:outline-none focus:ring-2 focus:ring-[#EA5C1F] rounded"
+						<a
+							className="hover:text-[#EA5C1F] focus:outline-none focus:ring-2 focus:ring-[#EA5C1F] rounded"
 							href="/"
-                     aria-label="Go to Services page"
-                  >
+							aria-label="Go to Services page"
+						>
 							Services
 						</a>
 					</li>
 
 					<li>
-						<a className="hover:text-[#EA5C1F] focus:outline-none focus:ring-2 focus:ring-[#EA5C1F] rounded"
+						<a
+							className="hover:text-[#EA5C1F] focus:outline-none focus:ring-2 focus:ring-[#EA5C1F] rounded"
 							href="/"
-                     aria-label="Go to pages"
-                  >
+							aria-label="Go to pages"
+						>
 							Pages
 						</a>
 					</li>
 
 					<li>
-						<a className="hover:text-[#EA5C1F] focus:outline-none focus:ring-2 focus:ring-[#EA5C1F] rounded"
+						<a
+							className="hover:text-[#EA5C1F] focus:outline-none focus:ring-2 focus:ring-[#EA5C1F] rounded"
 							href="/"
-                     aria-label="Go to contact page"
-                  >
+							aria-label="Go to contact page"
+						>
 							Contact
 						</a>
 					</li>
