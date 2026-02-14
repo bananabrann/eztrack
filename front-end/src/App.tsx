20f1eb15f9f9932eb5efd85caec82b17ef99eda7
FernandoDLT <ferchodlt1971@gmail.com>
2026-02-14 11:11:57 -0600

    EZ-15: Tools Management page UI with responsive grid and accessibility

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import ToolsManagement from "./components/ToolsManagement";
 
 function App() {
 	return (
 		<>
 			<Header />
+			<ToolsManagement />
 			<div className="flex justify-center items-center h-screen">
 				<img src="/eztrack-logo.png" alt="eztrack logo" className="max-w-md" />
 			</div>
