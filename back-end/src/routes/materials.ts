71127adff2140202f75f8f487f79eb3578dd3604
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2026-02-15 17:30:36 -0500

    feat: add POST /api/materials

--- back-end/src/routes/materials.ts
+++ back-end/src/routes/materials.ts
@@ -2,10 +2,51 @@ import { Router } from "express";
 
 import { materials } from "../faked/materials";
 
+const randUUID = crypto.randomUUID();
+
 const router = Router();
 // GET materials endpoint
 router.get("/", (req, res) => {
 	return res.status(200).json(materials);
+});
+
+// POST materials endpoint
+router.post("/", (req, res) => {
+	const { name, unitQty, unitCost, lowStockThreshold, projectId } =
+		req.body ?? {};
+
+	if (name.trim().length === 0 || typeof name !== "string") {
+		return res.status(400).json({ error: "Invalid name" });
+	}
+
+	if (projectId.trim().length === 0 || typeof projectId !== "string") {
+		return res.status(400).json({ error: "Invalid projectId" });
+	}
+
+	if (typeof unitQty !== "number" || unitQty < 0) {
+		return res.status(400).json({ error: "Invalid unitQty" });
+	}
+
+	if (typeof unitCost !== "number" || unitCost < 0) {
+		return res.status(400).json({ error: "Invalid unitCost" });
+	}
+
+	if (typeof lowStockThreshold !== "number" || lowStockThreshold < 0) {
+		return res.status(400).json({ error: "Invalid lowStockThreshold" });
+	}
+
+	const newMaterial = {
+		id: randUUID,
+		name: name.trim(),
+		unitQty,
+		unitCost,
+		lowStockThreshold,
+		projectId: projectId.trim(),
+	};
+
+	materials.push(newMaterial);
+
+	return res.status(200).json(newMaterial);
 });
 
 /**
