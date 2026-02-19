87417ba8f1fe3d1201c4d1eb9c17e998dad35cfe
FernandoDLT <ferchodlt1971@gmail.com>
2026-02-19 15:23:50 -0600

    Run Prettier to fix formatting

--- front-end/src/layouts/Layout.tsx
+++ front-end/src/layouts/Layout.tsx
@@ -2,10 +2,10 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 
 export default function Layout() {
-   return (
-      <>
-         <Header />
-         <Outlet />
-      </>
-   );
+	return (
+		<>
+			<Header />
+			<Outlet />
+		</>
+	);
 }
