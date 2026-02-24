622f017f05d1e7810d9b0b019e6b5980e6144988
Lisa Copeland <lisacopelandswe@gmail.com>
2026-02-24 00:24:05 -0500

    fix: [EZ-06] Fix lint runtime error

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -3,45 +3,42 @@ import { Logo } from "./Logo";
 import { LogOut } from "lucide-react";
 
 export default function Header() {
+	return (
+		<header className="navbar bg-tertiary shadow-sm px-6">
+			{/* LEFT: Logo */}
+			<div className="navbar-start">
+				<NavLink to="/" className="flex items-center">
+					<Logo size="md" />
+				</NavLink>
+			</div>
 
-  return (
-    <header className="navbar bg-tertiary shadow-sm px-6">
-      {/* LEFT: Logo */}
-      <div className="navbar-start">
-        <NavLink to="/" className="flex items-center">
-          <Logo size="md" />
-        </NavLink>
-      </div>
+			{/* CENTER: Links */}
+			<div className="navbar-center hidden md:flex">
+				<nav className="flex items-center gap-6">
+					<NavLink to="/" className="whitespace-nowrap">
+						Home
+					</NavLink>
 
-      {/* CENTER: Links */}
-      <div className="navbar-center hidden md:flex">
-        <nav className="flex items-center gap-6">
-            <NavLink to="/" className="whitespace-nowrap">
-              Home
-            </NavLink>
+					<NavLink to="/" className="whitespace-nowrap">
+						Tools Management
+					</NavLink>
 
-            <NavLink to="/" className="whitespace-nowrap">
-              Tools Management
-            </NavLink>
+					<NavLink to="/" className="whitespace-nowrap">
+						Materials Management
+					</NavLink>
 
-            <NavLink to="/" className="whitespace-nowrap">
-              Materials Management
-            </NavLink>
+					<NavLink to="/" className="whitespace-nowrap">
+						Project Management
+					</NavLink>
+				</nav>
+			</div>
 
-            <NavLink to="/" className="whitespace-nowrap">
-              Project Management
-            </NavLink>
-        </nav>
-      </div>
-
-      {/* RIGHT: User Logout */}
-      <div className="navbar-end">
-        <button
-          className="btn btn-sm btn-outline"
-        >
-          <LogOut className="w-8 h-8 mr-2" />
-        </button>
-      </div>
-    </header>
-  );
+			{/* RIGHT: User Logout */}
+			<div className="navbar-end">
+				<button className="btn btn-sm btn-outline">
+					<LogOut className="w-8 h-8 mr-2" />
+				</button>
+			</div>
+		</header>
+	);
 }
