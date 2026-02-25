7f7c3bdb0734851fe9e257f029c8fd3fd8d2b4d4
Lisa Copeland <lisacopelandswe@gmail.com>
2026-02-24 20:13:30 -0500

    fix: [EZ-06] Added aria labels to buttons and links

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -7,27 +7,24 @@ export default function Header() {
 		<header className="navbar bg-tertiary shadow-sm px-6">
 			{/* LEFT: Logo */}
 			<div className="navbar-start">
-				<NavLink to="/" className="flex items-center">
-					<Logo size="md" />
+				<NavLink to="/" className="flex items-center" aria-label="Go to Home Page">
+					<Logo size="sm" />
 				</NavLink>
 			</div>
 
 			{/* CENTER: Links */}
 			<div className="navbar-center hidden md:flex">
 				<nav className="flex items-center gap-6">
-					<NavLink to="/" className="whitespace-nowrap">
+					<NavLink to="/" className="whitespace-nowrap" aria-label="Go to Home Page">
 						Home
 					</NavLink>
-
-					<NavLink to="/" className="whitespace-nowrap">
+					<NavLink to="/" className="whitespace-nowrap" aria-label="Go to Tools Management">
 						Tools Management
 					</NavLink>
-
-					<NavLink to="/" className="whitespace-nowrap">
+					<NavLink to="/" className="whitespace-nowrap" aria-label="Go to Materials Management">
 						Materials Management
 					</NavLink>
-
-					<NavLink to="/" className="whitespace-nowrap">
+					<NavLink to="/" className="whitespace-nowrap" aria-label="Go to Project Management">
 						Project Management
 					</NavLink>
 				</nav>
@@ -35,7 +32,10 @@ export default function Header() {
 
 			{/* RIGHT: User Logout */}
 			<div className="navbar-end">
-				<button className="btn btn-sm btn-outline">
+				<button
+					className="inline-flex items-center rounded-md border border-current px-3 py-1.5 text-sm leading-none hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-current focus:ring-offset-2"
+					aria-label="Log Out"
+				>
 					<LogOut className="w-8 h-8 mr-2" />
 				</button>
 			</div>
