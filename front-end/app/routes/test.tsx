7bf434658bfa757ee4e6d07654f8cd8c7d042c2a
FernandoDLT <ferchodlt1971@gmail.com>
2026-02-26 17:40:22 -0600

    Fix Prettier Formatting

---
+++ front-end/app/routes/test.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  
+  return (
+    <div style={{ padding: "50px", textAlign: "center", fontFamily: "sans-serif" }}>
+      <h1 style={{ color: "#2563eb" }}>Test Route Active!</h1>
+      <p>Login Bypass</p>
+    </div>
+  );
+}
---
+++ front-end/src/components/ToolsCheck.tsx
@@ -0,0 +1,9 @@
+import ToolsManagement from "../../src/components/ToolsManagement";
+
+export default function ToosCheckRoute() {
+	return (
+		<main className="p-6">
+			<ToolsCheck />
+		</main>
+	);
+}
