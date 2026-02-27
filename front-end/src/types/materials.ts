f955b71037a5e4e0aed47b3c652a8e9fe8a429a5
Cindy Mae Ngoho <cindymaen17@gmail.com>
2026-02-27 18:26:28 -0500

    added the material usage

--- front-end/src/types/materials.ts
+++ front-end/src/types/materials.ts
@@ -1,8 +1,22 @@
+/**
+ * Types interface for the materials
+ */
 export interface Material {
 	id: string;
 	name: string;
-	unitQty: number;
-	unitCost: number;
-	lowStockThreshold: number;
-	projectId: string;
+	unit_qty: number;
+	unit_cost: number;
+	low_stock_threshold: number;
+	project_id: string;
+}
+
+/**
+ * Types interface for the material usage
+ */
+export interface MaterialUsage {
+	id: string;
+	material_id: string;
+	project_id: string;
+	quantity_used: number;
+	total_cost: number;
 }
