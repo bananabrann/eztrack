ea4d83d86700545db3f6622278161ffbc0857fa0
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2026-02-28 16:48:19 -0500

    fix: removed unused files

--- front-end/app/routes/test.tsx
+++
@@ -1,9 +0,0 @@
-export default function TestPage() {
-  
-  return (
-    <div style={{ padding: "50px", textAlign: "center", fontFamily: "sans-serif" }}>
-      <h1 style={{ color: "#2563eb" }}>Test Route Active!</h1>
-      <p>Login Bypass</p>
-    </div>
-  );
-}
--- front-end/src/components/ToolsCheck.tsx
+++
@@ -1,9 +0,0 @@
-import ToolsManagement from "../../src/components/ToolsManagement";
-
-export default function ToosCheckRoute() {
-	return (
-		<main className="p-6">
-			<ToolsCheck />
-		</main>
-	);
-}
