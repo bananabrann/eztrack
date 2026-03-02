1550973da2d28bd05d3055ac8b6c628b07389660
Ike Nwanze <nwanzeike@gmail.com>
2026-03-02 01:13:16 -0600

    [Frontend] EZ-63 Create Low Stock Section Component

--- front-end/src/routes/materials.tsx
+++ front-end/src/routes/materials.tsx
@@ -1,13 +1,10 @@
+import { LowStockSection } from "../components/LowStockSection";
+
 export default function Materials() {
 	return (
 		<div className="flex flex-col gap-6 items-center mt-8">
 			<h1 className="text-2xl font-bold">Materials</h1>
-			<div className="flex w-52 flex-col gap-4">
-				<div className="skeleton h-32 w-full"></div>
-				<div className="skeleton h-4 w-28"></div>
-				<div className="skeleton h-4 w-full"></div>
-				<div className="skeleton h-4 w-full"></div>
-			</div>
+			<LowStockSection />
 		</div>
 	);
 }
