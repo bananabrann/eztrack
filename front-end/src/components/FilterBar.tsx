57890fb65b6a1e05448bb99c7ea2c85999a6426e
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2026-03-01 21:27:22 -0500

    feat: create reusable SearchBar/FilterBar components

---
+++ front-end/src/components/FilterBar.tsx
@@ -0,0 +1,35 @@
+interface FilterOption {
+	value: string;
+	label: string;
+}
+
+interface FilterBarProps {
+	value: string;
+	onChange: (value: string) => void;
+	options: FilterOption[];
+	label?: string;
+}
+
+export function FilterBar({
+	value,
+	onChange,
+	options,
+	label = "Filter by",
+}: FilterBarProps) {
+	return (
+		<div className=" w-48 max-w-md mx-auto mb-6 flex items-center justify-center">
+			<select
+				value={value}
+				onChange={e => onChange(e.target.value)}
+				className="w-full border-[--tertiary-color] border rounded-lg px-4 py-2 focus:outline:none focus:ring-2 focus:ring-[--secondary-color]"
+			>
+				<option value="">{label}</option>
+				{options.map(o => (
+					<option key={o.value} value={o.value}>
+						{o.label}
+					</option>
+				))}
+			</select>
+		</div>
+	);
+}
