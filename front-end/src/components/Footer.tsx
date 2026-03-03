40393d8182b1600151d1eeba7da05c78c8a2296a
FernandoDLT <ferchodlt1971@gmail.com>
2026-03-02 22:58:58 -0600

    EZ-68: Add Footer component and render it in root.tsx

---
+++ front-end/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+	return (
+		<footer className="mt-auto bg-disabled text-center py-6 border-t border-gray-200">
+			<p className="text-sm text-gray-500">
+				© 2026 EzTrack, Inc. All rights reserved.
+			</p>
+		</footer>
+	);
+}
