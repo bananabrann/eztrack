ce6f6f4feb59ca678b3044b229e0f9cc18f53031
Lisa Copeland <lisacopelandswe@gmail.com>
2026-03-03 18:52:15 -0500

    fix: [EZ-72] Setup Create Project form

--- front-end/src/routes/projects.tsx
+++ front-end/src/routes/projects.tsx
@@ -15,6 +15,9 @@ export default function Projects() {
 	const [projects, setProjects] = useState<Project[]>([]);
 	const [loading, setLoading] = useState<boolean>(true);
 	const [error, setError] = useState<string | null>(null);
+	const [newProjectName, setNewProjectName] = useState("");
+	const [startDate, setStartDate] = useState("");
+	const [endDate, setEndDate] = useState("");
 
 	useEffect(() => {
 		const fetchProjects = async () => {
@@ -108,11 +111,47 @@ export default function Projects() {
 			/>
 			<dialog id="new_project_modal" className="modal modal-bottom sm:modal-middle">
 				<div className="modal-box">
-					<h3 className="font-bold text-lg">Hello!</h3>
-					<p className="py-4">Press ESC key or click the button below to close</p>
+					<h3 className="font-bold text-lg">Create New Project</h3>
 					<div className="modal-action">
-						<form method="dialog">
-							{/* if there is a button in form, it will close the modal */}
+						<form method="dialog" className="ml-5">
+							{/* Project Name */}
+							<input
+								type="text"
+								placeholder="Project Name"
+								className="input input-bordered w-full max-w-xs mb-4"
+								value={newProjectName}
+								onChange={event => setNewProjectName(event.target.value)}
+							/>
+							{/* Start Date */}
+							<label className="input">
+								<span className="label">Start Date</span>
+								<input
+									type="date"
+									className="input input-bordered mt-4 mb-5"
+									placeholder="Start Date"
+									value={startDate}
+									onChange={event => setStartDate(event.target.value)}
+								/>
+							</label>
+							{/* End Date */}
+							<label className="input">
+								<span className="label">End Date</span>
+								<input
+									type="date"
+									className="input input-bordered mt-4 mb-5"
+									value={endDate}
+									onChange={event => setEndDate(event.target.value)}
+								/>
+							</label>
+							<div className="mt-4 mb-4">
+								<Button
+									label="Save Project"
+									variant="blue"
+									type="submit"
+									onClick={() => {
+										//save form
+									}} />
+							</div>
 							<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
 						</form>
 					</div>
