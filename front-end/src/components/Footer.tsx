c0af7a974ef67fcc92548a2f382ce4cf23602c74
FernandoDLT <ferchodlt1971@gmail.com>
2026-03-03 18:05:59 -0600

    fernando: EZ-68: TEMP fix Materials page + minor Footer update

--- front-end/src/components/Footer.tsx
+++ front-end/src/components/Footer.tsx
@@ -1,10 +1,5 @@
 export default function Footer() {
 	return (
-		// <footer className="mt-auto bg-disabled text-center py-6 border-t border-gray-200">
-		// 	<p className="text-sm text-gray-500">
-		// 		© 2026 EzTrack, Inc. All rights reserved.
-		// 	</p>
-		// </footer>
 		<footer className="bg-tertiary py-6 border-t border-gray-200 w-full">
 			<div className="max-w-7xl mx-auto px-4 text-right">
 				<p className="text-sm text-white">
