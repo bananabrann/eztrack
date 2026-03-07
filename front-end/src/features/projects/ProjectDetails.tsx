eef75bc056dddc40f7e438b00fafe6ddb015e97f
Cindy Mae Ngoho <cindymaen17@gmail.com>
2026-03-06 22:10:22 -0500

    added the tools table and empty state

--- front-end/src/features/projects/ProjectDetails.tsx
+++ front-end/src/features/projects/ProjectDetails.tsx
@@ -12,7 +12,7 @@ import ProjectDetailsTable, {
 } from "./ProjectDetailsTable";
 import { Button } from "../../components/Button";
 import { FilterBar } from "../../components/FilterBar";
-import { Building } from "lucide-react";
+import { Building, Building2, Hammer, Package, Wrench } from "lucide-react";
 import ProjectToolsTable from "./ProjectToolsTable";
 
 type ProjectDetailsProps = {
@@ -203,43 +203,44 @@ export default function ProjectDetails({ projectId }: ProjectDetailsProps) {
 					containerClassName="w-80"
 				/>
 			</div>
-			{/* <div className="max-w-4xl mx-auto flex justify-end mb-4">
-				<Button
-					label={
-						projectStatus === "COMPLETED"
-							? "Project Completed"
-							: "Complete Project"
-					}
-					variant="orange"
-					size="sm"
-					disabled={isCompleting || projectStatus === "COMPLETED"}
-					onClick={() => setIsConfirmModalOpen(true)}
-				/>
-			</div> */}
+
 			{viewType === "" ? (
 				<div className="flex justify-center mt-16">
 					<div className="card bg-base-100 w-full max-w-sm shadow-sm p-8">
 						<div className="text-center">
-							<Building
+							<Building2
 								size={64}
 								className="mx-auto mb-4 text-tertiary opacity-50"
 							/>
 							<h3 className="text-lg font-semibold text-primary mb-2">
-								What would you like to view?
+								Select what to view
 							</h3>
 							<p className="text-tertiary mb-6">
-								Use the dropdown above to choose Materials or Tools for this
+								Use the dropdown above to view materials or tools for this
 								project.
 							</p>
 						</div>
-						.
 					</div>
 				</div>
 			) : viewType === "materials" ? (
 				<>
+					{/* Empty State for materials  */}
 					{rows.length === 0 ? (
-						<div className="text-gray-500">
-							No materials found for this project.
+						<div className="flex justify-center mt-16">
+							<div className="card bg-base-100 w-full max-w-sm shadow-sm p-8">
+								<div className="text-center">
+									<Package
+										size={64}
+										className="mx-auto mb-4 text-tertiary opacity-50"
+									/>
+									<h3 className="text-lg font-semibold text-primary mb-2">
+										No materials yet
+									</h3>
+									<p className="text-tertiary mb-6">
+										Add a material to get started with this project.
+									</p>
+								</div>
+							</div>
 						</div>
 					) : (
 						<ProjectDetailsTable
@@ -252,8 +253,21 @@ export default function ProjectDetails({ projectId }: ProjectDetailsProps) {
 			) : (
 				<>
 					{tools.length === 0 ? (
-						<div className="text-gray-500">
-							No tools assigned to this project.
+						<div className="flex justify-center mt-16">
+							<div className="card bg-base-100 w-full max-w-sm shadow-sm p-8">
+								<div className="text-center">
+									<Hammer
+										size={64}
+										className="mx-auto mb-4 text-tertiary opacity-50"
+									/>
+									<h3 className="text-lg font-semibold text-primary mb-2">
+										No tools yet
+									</h3>
+									<p className="text-tertiary mb-6">
+										Add a tools to get started with this project.
+									</p>
+								</div>
+							</div>
 						</div>
 					) : (
 						<div className="overflow-x-auto">
