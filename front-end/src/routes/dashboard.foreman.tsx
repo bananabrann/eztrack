f017f9b309ce3549f57dcfa007157063265f31b8
Lisa Copeland <lisacopelandswe@gmail.com>
2026-03-11 20:22:47 -0400

    fix: [EZ-108] Update foreman image

--- front-end/src/routes/dashboard.foreman.tsx
+++ front-end/src/routes/dashboard.foreman.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 import { ModuleCard } from "../components/ModuleCard";
-import foreman from "../assets/foreman_cropped.png";
+import foreman from "../assets/foreman2_cropped.png";
 import { getSupabaseClient } from "../lib/supabase";
 
 import { Boxes, Calendar, Clock, FolderKanban, Toolbox } from "lucide-react";
